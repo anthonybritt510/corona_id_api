3920e0cbc4614e2155a70ec2856ff1ec157c0c97
anthonybritt510 <anthonybritt510@gmail.com>
2020-05-25 07:16:46 +0200

    Added create symlink feature

--- app/Http/Controllers/API/InstallController.php
+++ app/Http/Controllers/API/InstallController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Model\Notification;
-use Illuminate\Support\Facades\Auth;
 
 class InstallController extends Controller
 {
@@ -13,7 +11,8 @@ class InstallController extends Controller
     public $errorStatus = 202;
 
     public function createSymlink(Request $request) {
+        $target = storage_path();
+        $link = public_path('storage');
+        symlink($target, $link);
     }
-
-
 }
